fae1397938948b5cec647c04b71bba17330df20d
asenacengel <57203106+asenacengel@users.noreply.github.com>
2020-03-03 15:57:25 -0500

    NavigationSample

---
+++ src/test/java/com/automation/tests/day02/NavigationSample.java
@@ -0,0 +1,31 @@
+package com.automation.tests.day02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationSample {
+    public static void main(String[] args) throws Exception {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        String url = "http://google.com";
+        driver.get(url);
+        Thread.sleep(5000);
+
+        String url2 = "http://amazon.com";
+        driver.navigate().to(url2);
+        Thread.sleep(3000);
+
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        driver.navigate().forward();
+        Thread.sleep(3000);
+
+        driver.navigate().refresh();
+        Thread.sleep(3000);
+
+        driver.close();
+    }
+}
